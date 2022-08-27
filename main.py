e6461f1711ec207b5f723eb59260c6d2e567e03a
Osvaldo Antunes <meloosvaldo@hotmail.com>
2022-08-27 11:46:09 -0300

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Olá,mundo!Aprendendo Python \nEu sou Osvaldo Melo e tenho")
+a=2022
+b=1990
+niver=a-b
+print (niver)
+print ("anos de idade")
+print ("Sou Licenciado em Informática \t                            Gosto de Programação")
